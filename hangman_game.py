1530bc3a46140d912352d4264944e9288473c513
Thebekarani <thebekarani@gmail.com>
2026-06-18 19:23:48 +0530

    initializing attempts

--- hangman_game.py
+++ hangman_game.py
@@ -3,3 +3,7 @@ print("Welcome to Hangman game!")
 lst=["Apple", "Banana", "Grapes", "Mango", "Orange"]
 #stores a random word from the list
 word=random.choice(lst)
+#guessed letters
+guessed_letters=[]
+#number of attempts 
+attempts=6
