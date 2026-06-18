535ecea76eb4a45b93f0db53b6bd0de67b0f8efd
Thebekarani <thebekarani@gmail.com>
2026-06-18 19:31:08 +0530

    guessed letters and underscores

--- hangman_game.py
+++ hangman_game.py
@@ -7,3 +7,14 @@ word=random.choice(lst)
 guessed_letters=[]
 #number of attempts 
 attempts=6
+while attempts>0:
+    #display the word with guessed letters and underscores for unguessed letters
+    display_word=""
+    for letter in word:
+        if letter.upper() in guessed_letters:
+            display_word+=letter+" "
+        else:
+            display_word+="_ "
+    print(display_word)
+    
+   
